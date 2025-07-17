17cd46c0b1e3dd5911323289ce68b5cb04402961
Konda Sridhar <kondasridhar628@gmail.com>
2025-07-17 23:12:03 +0530

    fixing vercel deploy with serverless http

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 // api/index.js
-// server/server.js
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import serverless from "serverless-http"; // 👈 NEW
 
 import blogRoutes from "./routes/blogRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API is working!"));
-
 app.use("/api/blogs", blogRoutes);
 app.use("/api/ai", aiRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
-export default app; // ✅ Vercel needs this
+// 👇 Export wrapped Express handler
+export const handler = serverless(app);
+// ✅ Vercel needs this
  // for Vercel
 
 
