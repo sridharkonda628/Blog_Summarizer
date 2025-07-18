f6248311098ff75009dca98a3cdb2928e1d5286c
Konda Sridhar <kondasridhar628@gmail.com>
2025-07-18 16:47:17 +0530

    modified the routes of api/summarize

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { summarizeBlog } from "../controllers/aiController.js";
 
 const router = express.Router();
-router.post("/api/summarize", summarizeBlog);
+router.post("/", summarizeBlog);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -43,9 +43,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
+app.use(express.json());
 const allowedOrigins = [
   'http://localhost:5173', // for local dev
-  'https://blog-summarizer-n5hn.vercel.app/dashboard' // replace with your actual frontend Vercel domain
+  'https://blog-summarizer-n5hn.vercel.app' // replace with your actual frontend Vercel domain
 ];
 
 app.use(cors({
@@ -55,7 +56,6 @@ app.use(cors({
 }));
 
 
-app.use(express.json());
 
 // Routes
 app.get("/api", (req, res) => {
@@ -63,7 +63,7 @@ app.get("/api", (req, res) => {
 });
 
 app.use("/api/blogs", blogRoutes);
-app.use("/api/ai", aiRoutes);
+app.use("/api/summarize", aiRoutes);
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
